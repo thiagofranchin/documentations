cf09d1d8fd560efaa0a365456d65fd45b8d640c6
Thiago Franchin <thiago.franchin@gmail.com>
2019-08-01 00:26:18 -0300

    repository init

---
+++ js/chunk-2d21d18e.d12f83d9.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21d18e"],{d09e:function(e,n,t){"use strict";t.r(n);var r=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"sobre"},[t("h1",[e._v("Sobre")])])}],c=t("2877"),l={},u=Object(c["a"])(l,r,s,!1,null,null,null);n["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d21d18e.d12f83d9.js.map
